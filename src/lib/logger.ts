074deda16beac7778f24583b73b1ffc776478dee
diego029929 <diegoferraro@free.fr>
2025-09-01 14:52:19 +0200

    Create logger.ts

---
+++ src/lib/logger.ts
@@ -0,0 +1,11 @@
+import pino from "pino";
+import pinoHttp from "pino-http";
+
+export const logger = pino({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  transport: process.env.NODE_ENV === "production" ? undefined : {
+    target: "pino-pretty"
+  }
+});
+
+export const httpLogger = pinoHttp({ logger });
